830b943915e419b2381c2c823791a36a50194bfb
Daniela <danielapatriciabystrom@gmail.com>
2024-02-22 12:53:17 +0100

    Added todo-done-styling

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import Checkbox from "./Checkbox";
+import "./Todo.css";
 
 interface TodoProps {
   todo: string;
 }
 
 function Todo({ todo }: TodoProps) {
+  const [done, setDone] = useState(false);
+
+  const handleClick = () => {
+    setDone(!done);
+  };
+
   return (
-    <div className="flex items-center">
-      <div className="todo flex items-center">
+    <div
+      className={`flex items-center todo ${done ? "done" : ""}`}
+      onClick={handleClick}
+    >
+      <div className="">
         <Checkbox defaultChecked={false} />
         <span className="ml-2">{todo}</span>
       </div>
