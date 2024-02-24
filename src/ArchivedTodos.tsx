37a5f3dd04c8f6ec3f5afacd4fe451eceba8b508
Daniela <danielapatriciabystrom@gmail.com>
2024-02-24 15:38:29 +0100

    Added "back"-button on archived-page

--- src/ArchivedTodos.tsx
+++ src/ArchivedTodos.tsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 function ArchivedTodos() {
   return (
     <>
       <h1>HEJ</h1>
+      <Link to="/">
+        <button className="mr-2 rounded-2xl px-4 py-1 mt-5 text-slate-950 bg-white border-slate-50 ">
+          Tillbaka
+        </button>
+      </Link>
     </>
   );
 }
